cda683dad191914cfa10d0a2ab108fed98bc9874
Dfn2502 <115895102+Dfn2502@users.noreply.github.com>
2026-05-12 16:53:01 -0600

    Todo

---
+++ Java/script.js
@@ -0,0 +1,52 @@
+const modal = document.getElementById("modal");
+const modalImg = document.getElementById("modal-img");
+const modalVideo = document.getElementById("modal-video");
+const closeBtn = document.querySelector(".close");
+
+document.querySelectorAll(".btn-card").forEach(btn => {
+    btn.addEventListener("click", e => {
+
+        const href = btn.getAttribute("href");
+
+        // 🔵 externos
+        if (href && href.startsWith("http")) return;
+
+        e.preventDefault();
+
+        const src = btn.getAttribute("data-src");
+        if (!src) return;
+
+        // reset
+        modalImg.style.display = "none";
+        modalVideo.style.display = "none";
+
+        if (src.includes(".mp4")) {
+            modalVideo.src = src;
+            modalVideo.style.display = "block";
+        } else {
+            modalImg.src = src;
+            modalImg.style.display = "block";
+        }
+
+        modal.style.display = "flex";
+    });
+});
+
+closeBtn.addEventListener("click", () => {
+    modal.style.display = "none";
+    modalVideo.pause();
+});
+
+
+
+const track = document.querySelector(".carousel-track");
+
+document.querySelectorAll(".project-card").forEach(card => {
+    card.addEventListener("mouseenter", () => {
+        track.style.animationPlayState = "paused";
+    });
+
+    card.addEventListener("mouseleave", () => {
+        track.style.animationPlayState = "running";
+    });
+});
